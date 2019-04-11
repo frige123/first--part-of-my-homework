4830fd06ab8702424b85223caf4bdfb634725d49
frige123 <chopkake@gmail.com>
2019-04-11 22:31:43 +0300

    first part of my homework in index.html and index1.js

---
+++ index1.js
@@ -0,0 +1,21 @@
+var numb = 21; 
+var str = 'hello'; 
+var work = true; 
+var obj = {a:1,b:2,c:3}; 
+var arr = [1,2,3,4,5,6,7,8,9,0]; 
+function myfunk(){var a=12
+var b=13
+console.log(a+b)
+}
+var a;
+a   //  вызываем a и получаем undefine
+str - numb  // nan
+var f =12; 
+f 
+f = Null;
+
+
+ 
+ 
+
+
